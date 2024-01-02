36eefa4d21567892e2dc5d254a19b92e7e78785f
natewood2 <nathanwood.sfg@gmail.com>
2024-01-02 14:12:03 -0600

    fixed style error

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123)
-	if chr(i) not in ['e', 'q']).format(), end="")
+print("".join(
+    chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']
+).format(), end="")
