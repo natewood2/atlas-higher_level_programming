b310e5b577541e75ad0c0ed0baf5ab2c1538950c
natewood2 <nathanwood.sfg@gmail.com>
2024-01-03 13:55:40 -0600

    added da d

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,8 +4,8 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    a = 5
-    b = 10
+    a = 10
+    b = 5
 
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
