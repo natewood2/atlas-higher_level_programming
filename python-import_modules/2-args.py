4d470c6340c8bb1fe938e2d95c80a3bfde456324
natewood2 <nathanwood.sfg@gmail.com>
2024-01-03 14:21:40 -0600

    hope lets have hope

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    arg_c = len(sys.argv) - 1
+
+    argument = "argument" if arg_c == 1 else "arguments"
+
+    if arg_c == 0:
+        print("0 arguments.)
+    else:
+        print("Number of {argument}: {arg_c}:".format(argument, arg_c))
+
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
