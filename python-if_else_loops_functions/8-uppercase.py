d0fc3b76b18e873fadd4434688b1cda13f877b23
natewood2 <nathanwood.sfg@gmail.com>
2024-01-03 10:46:20 -0600

    uppercase is not making me happy

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper_str = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            upper_str += char(ord(str) - 32)
-        else:
-            upper_str += char
-    print(upper_str, end="")
-    print()
+            char = chr(ord(char) - 32)
+            print("{}".format(char), end="")
+        print("")
