cf8a8dcf3334bce81364418fbbd175d3f803b119
natewood2 <nathanwood.sfg@gmail.com>
2024-01-03 12:24:24 -0600

    indent

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def main():
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
 
 if __name__ == "__main__":
-    main()
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
