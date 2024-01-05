5623f4742dcdc87849e0097a790956a6d2220cdc
natewood2 <nathanwood.sfg@gmail.com>
2024-01-05 12:49:43 -0600

    maybe this will work

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,9 @@
 def new_in_list(my_list, idx, element):
     copy_l = my_list.copy()
 
-    if 0 < idx <= len(copy_l):
-        copy_l[idx] = element
-
-        return copy_l
+    if idx < 0 or  idx >= len(copy_l):
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    
+    return new_list
