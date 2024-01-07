c0fd1a9981cd2330c415c8ec89e31e0ea633aa4c
natewood2 <nathanwood.sfg@gmail.com>
2024-01-07 12:59:49 -0600

    was not expecting to have to make two files to work together, tripped me up

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_s = list(a_dictionary)
-    list_s.sort()
-    for key in list_s:
-        print("{}: {}".format(key, a_dictionary[key]))
+    if a_dictionary is not None:
+        list_s = list(a_dictionary)
+        list_s.sort()
+        for key in list_s:
+            print("{}: {}".format(key, a_dictionary[key]))
