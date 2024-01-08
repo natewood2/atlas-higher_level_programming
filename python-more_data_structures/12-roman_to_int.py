d2caa62fb05dd6f666406216d3a150fe25fc1e30
natewood2 <nathanwood.sfg@gmail.com>
2024-01-07 21:46:01 -0600

    NOne is now 0

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None:
-        return None
+        return 0
 
     if not isinstance(roman_string, str):
         return 0
