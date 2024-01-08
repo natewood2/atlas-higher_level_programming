e96f6f6c4ec6eabd721604337f9d326dd9e5206e
natewood2 <nathanwood.sfg@gmail.com>
2024-01-07 21:44:51 -0600

    success

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return None
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    updated_value = 0
+
+    for c in reversed(roman_string):
+        value = dict[c]
+        if value < updated_value:
+            num -= value
+            updated_value = value
+        else:
+            num += value
+        updated_value = value
+    return num
