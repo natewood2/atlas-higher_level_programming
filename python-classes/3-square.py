a59b546b2e66ef3ada027e4f2bdbd7b8ad591970
natewood2 <nathanwood.sfg@gmail.com>
2024-01-12 14:31:57 -0600

    return da square

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines a class """
+
+
+class Square:
+    """ Defines a plan for square """
+    def __init__(self, size=0):
+        error_ = "size must be an integer"
+        _error = "size must be >= 0"
+        if not isinstance(size, int):
+            raise TypeError(error_)
+        if size < 0:
+            raise ValueError(_error)
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
