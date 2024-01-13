46d8d775155788cc13858ccb9f417fe5971a2e8f
natewood2 <nathanwood.sfg@gmail.com>
2024-01-12 19:10:50 -0600

    fix pycode violation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ Defines a plan for square """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
