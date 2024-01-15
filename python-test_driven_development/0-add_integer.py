d1d0d98655e4004fc487f48aefd1ed702557671a
natewood2 <nathanwood.sfg@gmail.com>
2024-01-14 19:30:28 -0600

    fixed doctest

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Function that adds floats and ints """
+
+
 def add_integer(a, b=98):
+    """ Returns the addition or raises a type error depending on user input """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
