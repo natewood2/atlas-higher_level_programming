a89a81437f195bf7181f5795c3ee5ed3ed55cc1e
natewood2 <nathanwood.sfg@gmail.com>
2024-01-16 22:44:47 -0600

    fixed newline accident

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,18 +3,19 @@
 def text_indentation(text):
     """ Defines function """
     text_error = "text must be a string"
-    
+
     if not isinstance(text, str):
         raise TypeError(text_error)
-    
+
     empty_str = ""
-    
+
     for each_char in text:
         if each_char in ['.', '?', ':']:
             empty_str = empty_str + each_char
             trimmed_str = empty_str.strip()
             print(trimmed_str)
-            print("\n")
+            print()
             empty_str = ""
-        else:
-            empty_str = empty_str + each_char
+
+    if empty_str:
+        print(empty_str.strip())
