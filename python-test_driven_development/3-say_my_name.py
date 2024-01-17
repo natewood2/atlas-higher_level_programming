4e78e1f5579252de5689456369b48f8481223260
natewood2 <nathanwood.sfg@gmail.com>
2024-01-17 09:46:07 -0600

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+""" Documenting module """
+
+
 def say_my_name(first_name, last_name=""):
+    """ Say my name documentation """
     first_name_error = "first_name must be a string"
     last_name_error = "last_name must be a string"
-    
+
     if not isinstance(first_name, str):
         raise TypeError(first_name_error)
     if not isinstance(last_name, str):
         raise TypeError(last_name_error)
     full_name = first_name + " " + last_name
-    
+
     print(f"My name is {full_name}")
