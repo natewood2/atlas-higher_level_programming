00250426085255216a362c4586bfaeeb62d55358
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 20:54:28 -0600

    using super

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module contains and inherits from 9-rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defining new rectangle class"""
+    def __init__(self, size):
+        """ Init method """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    # def area(self):
+       # """ Area for a square """
+       # return self.__size ** 2
