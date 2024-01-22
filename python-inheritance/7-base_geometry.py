bd81f1047dfb1176194ec8dea69dc2109f74c626
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 19:18:04 -0600

    formatting formatting

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines non empty class """
+
+
+class BaseGeometry:
+    """ Class raises a expects """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
