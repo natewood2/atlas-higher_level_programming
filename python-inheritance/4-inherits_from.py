eb3495186a0e64418d683e2418b2760baccbb3aa
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 21:08:29 -0600

    fixed pycode violations

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,8 +3,8 @@
 
 
 def inherits_from(obj, a_class):
-    """  a function that returns True if the object is an instance of a subclass """
+    """function that returns True if the object is a subclass"""
     if type(obj) is a_class:
         return False
-    
+
     return isinstance(obj, a_class)
