e61c57b0d660e35418918f8b4857cb4b3e7403ef
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 20:31:03 -0600

    added documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" This module has the BaseGeometry """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """ Initializes a new rectangle """
     def __init__(self, width, height):
+        """ Init special method """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
