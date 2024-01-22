92ec2a3f981775192dab9713fbb26cc16fd13cd4
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 21:04:35 -0600

    need a harder flippin question

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module contains and inherits from 9-rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defining new rectangle class"""
+    def __init__(self, size):
+        """ Init method """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Returns the area of square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
