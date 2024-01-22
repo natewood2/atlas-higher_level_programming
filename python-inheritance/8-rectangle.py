bfd859cd2ff2c0c0f5a7d3327c6f0741ce9fd0e5
natewood2 <nathanwood.sfg@gmail.com>
2024-01-21 20:01:01 -0600

    this seems complicated for no reason

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-""" Defines non empty class """
-
-
-class BaseGeometry:
-    """ Class raises a expects """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Initializes a new rectangle """
