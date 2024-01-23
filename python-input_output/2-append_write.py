553eee2fdb03d29adf0539874e115b9515683a52
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 13:37:10 -0600

    no clue

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """Appending to the end of a file"""
     with open(filename, 'a', encoding='utf-8') as f:
-        f.write(text, end="")
+        f.write(text)
         return len(text)
