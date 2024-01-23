1c8c51de68cf1db4239a49b990b56a66da112e08
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 13:33:37 -0600

    we getting through this... I hope

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a module that appends a string"""
+
+
+def append_write(filename="", text=""):
+    """Appending to the end of a file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text + '\n')
+        return len(text)
