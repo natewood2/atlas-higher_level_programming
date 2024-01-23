322c6807c36bac6ebe69c88b8c66a54b540e6e27
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 14:46:57 -0600

    removed .py from my imported files

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ import sys
 import os.path
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file= __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file= __import__('6-load_from_json_file').load_from_json_file
 
 """Adds everything to empty list"""
 empty_list = []
