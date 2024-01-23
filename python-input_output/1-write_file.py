6554e654229d8124dd888fe56b501195989edcc7
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 13:14:31 -0600

    it worked but i have pycode violation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """ Opening a file and returns the amount of Char"""
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(text)
-        return(len(text))
+        return (len(text))
