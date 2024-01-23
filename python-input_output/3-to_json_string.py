d8550824f8e6b6539ff043f8f8061049d7b2e3da
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 13:50:10 -0600

    it might have been easier than I thought

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,5 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """JSON representation of obj"""
-    with open('output.json', 'w') as json_file:
-        json.dump(my_obj, json_file)
+    return json.dump(my_obj)
