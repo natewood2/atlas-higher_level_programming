9eaaad3b2f47701d524d110b6230d592ab6e30d8
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 14:41:52 -0600

    that was not fun

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Saves files module"""
+import sys
+import os.path
+
+
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file= __import__('6-load_from_json_file.py').load_from_json_file
+
+"""Adds everything to empty list"""
+empty_list = []
+list_of_arguments = sys.argv[1:]
+if os.path.exists("add_item.json"):
+    empty_list = load_from_json_file("add_item.json")
+save_to_json_file(empty_list + list_of_arguments, "add_item.json")
+
