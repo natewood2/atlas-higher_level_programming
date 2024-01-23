9431dfa41bd5b96a0ef123c4e822ee81fced796f
natewood2 <nathanwood.sfg@gmail.com>
2024-01-23 13:11:01 -0600

    testing to see if it works

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """ Opening a file and returns the amount of Char"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+        return(len(text))
+        
