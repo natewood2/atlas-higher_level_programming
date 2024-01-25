948576204c0ec82836d45e63732a645d8b6ffb7c
natewood2 <nathanwood.sfg@gmail.com>
2024-01-25 12:15:12 -0600

    maybe this is correct?

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,28 +7,13 @@ class Rectangle(Base):
     """Rect class that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
 
         self.__width = width
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
+
         self.__height = height
 
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x <= 0:
-            raise ValueError("x must be >= 0")
         self.__x = x
 
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y <= 0:
-            raise ValueError("y must be >= 0")
         self.__y = y
 
     @property
@@ -55,7 +40,7 @@ class Rectangle(Base):
         """Defines setter"""
         if height <= 0:
             raise ValueError("height must be > 0")
-        if not isinstance(value, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         self.__height = height
 
@@ -84,5 +69,5 @@ class Rectangle(Base):
         if y <= 0:
             raise ValueError("y must be >= 0")
         if not isinstance(y, int):
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         self.__y = y
