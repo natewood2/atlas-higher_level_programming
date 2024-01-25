3ac9d5e4a42537fa360bb826d7f72f82cf55e50f
natewood2 <nathanwood.sfg@gmail.com>
2024-01-25 14:44:16 -0600

    SQUAFRE

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,4 +9,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.size)
