5b3b11e322f1b55f47c01dde3b043b7403fae862
natewood2 <nathanwood.sfg@gmail.com>
2024-01-25 12:23:59 -0600

    less than but not equal

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -69,7 +69,7 @@ class Rectangle(Base):
         """Defines x"""
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
-        if x <= 0:
+        if x < 0:
             raise ValueError("x must be >= 0")
         self.__x = x
 
@@ -83,6 +83,6 @@ class Rectangle(Base):
         """Defines y setter"""
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
-        if y <= 0:
+        if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
