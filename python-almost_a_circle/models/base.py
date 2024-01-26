a2db09f0b6a9e4e0906d62e2d174e13888c0b8ae
natewood2 <nathanwood.sfg@gmail.com>
2024-01-26 12:02:19 -0600

    pushing up json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a modules"""
+import json
 
 
 class Base:
@@ -12,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """standard formats for sharing data representation."""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
