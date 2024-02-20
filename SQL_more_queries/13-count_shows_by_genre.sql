a6e5c7bd5ddd459b0306b9382a044e91f0e41d2a
root <nathanwood.sfg@gmail.com>
2024-02-20 00:58:36 +0000

    Adding 13, I couldnt check it because Import didn't work again

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- Count Shows By Genre
-SELECT DISTINCT g.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM g
-INNER JOIN tv_show_genres ON g.id = tv_show_genres.genre_id 
-GROUP BY genre 
+SELECT DISTINCT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre 
 ORDER BY number_of_shows DESC;
