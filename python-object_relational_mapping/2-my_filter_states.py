bc08d43fc770d11ed39a9408a3258ba25123ebe4
natewood2 <nathanwood.sfg@gmail.com>
2024-02-20 13:32:08 -0600

    Adding 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Script that displays all values in the states table where name matches the argument. """
+""" 
+Script that displays all values in the states 
+table where name matches the argument.
+"""
 
 import MySQLdb
 import sys
 
 
 def sql_connect(usr, pw, db_name, state_name):
-    """ Connect to a MySQL database and display all states matching a specific name. """
+    """ 
+    Connect to a MySQL database and display 
+    all states matching a specific name. 
+    """
 
     db = MySQLdb.connect(host="localhost",
                          user=usr,
@@ -15,7 +21,8 @@ def sql_connect(usr, pw, db_name, state_name):
                          port=3306)
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+    query = "SELECT * FROM states WHERE name = '{}'\
+        ORDER BY id ASC;".format(state_name)
     cur.execute(query)
 
     rows = cur.fetchall()
