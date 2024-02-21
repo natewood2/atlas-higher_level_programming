26f46857d9006783d6a31df9d2ea806c3d958e0d
natewood2 <nathanwood.sfg@gmail.com>
2024-02-20 19:40:05 -0600

    Models

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,13 +11,7 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class State(Base):
-    """
-    Defines the class State that inherits from Base
-    """
+    """ State Class. """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-engine = create_engine('mysql+mysqldb://<username>:<password>@localhost:3306/<dbname>', pool_pre_ping=True)
-
-Base.metadata.create_all(engine)
