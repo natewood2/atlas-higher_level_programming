0dc235729295f41ada39bfc7833029eb13a9a648
natewood2 <nathanwood.sfg@gmail.com>
2024-02-20 19:55:31 -0600

    Adding 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that prints the first State object from hbtn_0e_6_usa.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
