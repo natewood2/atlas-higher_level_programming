b277f15e7a57fbd4b978b365b26c6d1ccc1c3b7a
natewood2 <nathanwood.sfg@gmail.com>
2024-03-21 18:06:53 -0500

    Adding Task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,4 @@ const myVar = 'C is fun';
 const myVar2 = 'Python is cool';
 const myVar3 = 'JavaScript is amazing';
 
-console.log(myVar + "\n" + myVar2 + "\n" + myVar3);
+console.log(myVar + '\n' + myVar2 + '\n' + myVar3);
