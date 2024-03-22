c43cf96bf97db35afcfe5eae17ed6d1d766fc8ef
natewood2 <nathanwood.sfg@gmail.com>
2024-03-21 21:52:54 -0500

    Adding task 3

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const printArg = args[0];
+
+if (printArg === undefined) {
+    console.log('No argument');
+} else {
+    console.log(printArg);
+};
