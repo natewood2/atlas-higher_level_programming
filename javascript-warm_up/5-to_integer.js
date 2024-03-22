df8b4ac7e1cd3bedb5351c3c09b6026792077e10
natewood2 <nathanwood.sfg@gmail.com>
2024-03-22 15:11:37 -0500

    Only two spaces

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const ArgisInt = parseInt(args[0], 10);
 
 if (isNaN(ArgisInt)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  console.log(`My number: ${ArgisInt}`)
+  console.log(`My number: ${ArgisInt}`);
 }
