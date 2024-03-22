3396656b6eabc470d45278adf021193fe576ac9d
natewood2 <nathanwood.sfg@gmail.com>
2024-03-21 21:34:59 -0500

    Added two

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,15 +2,11 @@
 const arguments = process.argv.slice(2);
 
 if (process.argv.length === 2) {
-    console.log('No argument')
+    console.log('No argument');
 } else if (process.argv.length === 3) {
     console.log('Argument found');
-};
+}
 
-if (process.argv.length === 4) {
-    console.log('Argument found');
-};
-
-if (process.argv.length === 5) {
+if (process.argv.length > 3) {
     console.log('Arguments found');
-};
+}
