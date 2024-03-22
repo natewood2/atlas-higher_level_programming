516813c489b3bcd5c6dac46f80d3beebf81f0ec1
natewood2 <nathanwood.sfg@gmail.com>
2024-03-21 22:01:20 -0500

    Javascript is so picky

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const args = process.argv.slice(2);
 
-if (args.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-}
-
-if (process.argv.length > 3) {
-  console.log('Arguments found');
-}
+if (args.length === 0) {
+    console.log('No argument');
+  } else if (args.length === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
