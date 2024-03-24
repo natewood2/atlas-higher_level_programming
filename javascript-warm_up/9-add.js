fd1866320a1232233c0186058c0871dcc27645d4
natewood2 <nathanwood.sfg@gmail.com>
2024-03-23 21:12:43 -0500

    Adding 9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+function add(a, b) {
+  return a + b;
+}
+
+const numbersEntered = process.argv.slice(2);
+
+const number = parseInt(numbersEntered[0], 10);
+const number2 = parseInt(numbersEntered[1], 10);
+
+const sum = add(number, number2)
+
+console.log(sum)
