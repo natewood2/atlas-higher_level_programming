376d7e20194bb67d9362efa0d166a8f2f9e61d4f
natewood2 <nathanwood.sfg@gmail.com>
2024-03-26 13:19:05 -0500

    Adding 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends Rectangle {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
