a8c72ea12053cbbe83c4241bf9708e52336a278c
natewood2 <nathanwood.sfg@gmail.com>
2024-04-01 07:11:15 -0500

    Adding error handling

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const request = require('request');
 const movieTitle = process.argv[2];
 const api = `https://swapi-api.hbtn.io/api/films/${movieTitle}`;
+
+if (!movieTitle) {
+  console.log("Please enter movie ID.");
+  process.exit(1);
+}
 
 request(api, { json: true }, (error, response, body) => {
   console.log(body.title);
