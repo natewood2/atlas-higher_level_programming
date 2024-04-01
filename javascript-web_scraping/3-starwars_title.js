e54887bb71ede8404e1286844542cfbc9f8a63bf
natewood2 <nathanwood.sfg@gmail.com>
2024-04-01 07:19:31 -0500

    Add

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const movieTitle = process.argv[2];
-const api = `https://swapi-api.hbtn.io/api/films/${movieTitle}`;
+const movieTitlee = process.argv[2];
+const api = `https://swapi-api.hbtn.io/api/films/${movieTitlee}`;
 
-if (!movieTitle) {
+if (!movieTitlee) {
   console.log("Please enter movie ID.");
   process.exit(1);
 }
