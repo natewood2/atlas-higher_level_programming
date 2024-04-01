e57a20d0bae47dcaa84c1e3decb00d5f05d33593
natewood2 <nathanwood.sfg@gmail.com>
2024-04-01 07:08:10 -0500

    Fixed indentation

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const movie_title = process.argv[2];
-const api = `https://swapi-api.hbtn.io/api/films/${movie_title}`;
+const movieTitle = process.argv[2];
+const api = `https://swapi-api.hbtn.io/api/films/${movieTitle}`;
 
 request(api, { json: true }, (error, response, body) => {
   console.log(body.title);
