cf6a30bd1792a50101893605a8a13e3d41e414ae
natewood2 <nathanwood.sfg@gmail.com>
2024-04-02 17:41:25 -0500

    Lorip sum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const request = require('request');
+
+const [url, filePath] = process.argv.slice(2);
+
+request(url, (error, response, body) => {
+  fs.writeFile(filePath, body, 'utf8', () => {});
+});
