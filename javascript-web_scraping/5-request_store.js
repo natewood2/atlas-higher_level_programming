4ba12d21cb5598e3642bf1ce03c053dcf90ff37b
natewood2 <nathanwood.sfg@gmail.com>
2024-04-02 17:47:31 -0500

    Adding err handling

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,9 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const fs = require('fs');
 const request = require('request');
 
 const [url, filePath] = process.argv.slice(2);
 
-request(url, (error, response, body) => {
+request(url, (err, response, body) => {
   fs.writeFile(filePath, body, 'utf8', () => {});
+  if (err) {
+    console.log(err);
+  }
 });
